ad55f78a478e68e44dba69ac150bcc851d7a87c1
Manuel Carbajo <mcarbajo@hl-codon-09-02.ebi.ac.uk>
2023-11-22 10:51:49 +0000

    helps parallelise fastaq downloads without spam

--- scripts/fastq_download.py
+++ scripts/fastq_download.py
@@ -1,30 +1,38 @@
+import sys
+import requests
+import os
+import subprocess
+from datetime import datetime
+import my_process as mp
+import random
+import time
 
 
-def download_fastq(fastq_file, baseDir):
+def download_fastq(fastq_file, genome_dir, baseDir):
     log_file_path = os.path.join(baseDir, 'rna_script.log')
     with open(log_file_path, 'w') as log_file:
         try:
-            
-            download_fastq_command = ["perl",baseDir + "/frameworks/ensembl-hive/scripts/standaloneJob.pl","Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveDownloadRNASeqFastqs","-ftp_base_url","ftp://ftp.sra.ebi.ac.uk/vol1/fastq/","-input_dir","short_read_fastq_dir", "-iid",fastq_file]
+            download_fastq_command = ["perl", baseDir + "/frameworks/ensembl-hive/scripts/standaloneJob.pl","Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveDownloadRNASeqFastqs","-ftp_base_url","ftp://ftp.sra.ebi.ac.uk/vol1/fastq/","-input_dir","." , "-iid",fastq_file]
             subprocess.run(download_fastq_command, stdout=log_file, stderr=subprocess.STDOUT, check=True)
-            print(fastq_file + " downloaded " )
-            #current_rank = "level_" + str(l) + "_rank"
-            #genome_tax_rank = tax_ranks[current_rank]
-            #tax_hierarchy = tax_ranks[current_hierarchy]
-            #print(genome + " " + str(genome_tax) + genome_tax_rank + " hierarchy: " + str(tax_hierarchy))
             
         except subprocess.CalledProcessError as e:
-            print("Download fastq error. Command :"+ str(command) +": " + str(e) +" ")
+            print("Download fastq error. Command :"+ str(download_fastq_command) +": " + str(e) +" ")
               
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         print("The path to the genome is missing as an argument.")
     else:
-        genome_name = sys.argv[1]
+        # Generate a random wait time between 0 and 15 seconds to avoid spaming ENA
+        wait_time = random.randint(1, 1500) / 100.0
+        time.sleep(wait_time)
+        
+        genome_dir = sys.argv[1]
         fastq_file = sys.argv[2]
         baseDir = sys.argv[3]
-        print("-- genome_name: " + genome_name +"\t fastq_file: " + fastq_file )
-      
-        #download_fastq( fastq_file, baseDir)
-
+        now1 = datetime.now()
+        #print("-- genome_dir: " + genome_dir +"\t fastq_file: " + fastq_file + " " + str(now1.strftime('%Y-%m-%d %H:%M:%S')))
+        download_fastq( fastq_file, genome_dir, baseDir)
+        now2 = datetime.now()
+        lap = now2 - now1
+        print(fastq_file + " downloaded. Elapsed time: " + str(lap) )
