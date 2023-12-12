af79650220beca09de3de3145376a36709220b60
Manuel Carbajo <mcarbajo@hl-codon-35-04.ebi.ac.uk>
2023-12-12 14:55:45 +0000

    wraps mysql query inside try block

--- scripts/rfam_accessions.py
+++ scripts/rfam_accessions.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from datetime import datetime
 import mysql.connector
 import my_process as mp
@@ -8,8 +9,10 @@ def query_Rfam(tax_ranks, config_file_path, genome_dir):
     # Read MySQL connection parameters from the configuration file
     host, user, password, database, port = mp.read_config(config_file_path)
     genome_name = tax_ranks["genome_name"]
-    rfam_ids_path = "rfam_" + genome_name + "_ids.txt"
-
+    current_dir = os.getcwd()
+    print("current_dir: " + current_dir)
+    rfam_ids_path = "rfam_ids.txt"
+    rfam_results = []
     # Establish a connection to the MySQL database
     try:
         connection = mysql.connector.connect(
@@ -23,54 +26,53 @@ def query_Rfam(tax_ranks, config_file_path, genome_dir):
         # Create a cursor object to interact with the database
         cursor = connection.cursor()
         max_nb_families = 0
-        rfam_result = []
-
+        rfam_result = set()
         for l in range(4):
             name_tag = "level_" + str(l) + "_name"
             tax_tag = "level_" + str(l) + "_tax"
             rank_tag = "level_" + str(l) + "_rank"
-        
-            if tax_ranks[rank_tag]:
-                genome_name = tax_ranks[name_tag]
-                rank = tax_ranks[rank_tag] 
-                rank_hierarchy = mp.ranks_dict[rank]
-                if rank_hierarchy <= mp.ranks_dict['species']:
+            try:
+                if tax_ranks[rank_tag]:
+                    genome_name = tax_ranks[name_tag]
+                    rank = tax_ranks[rank_tag] 
+                    rank_hierarchy = mp.ranks_dict[rank]
+                    if rank_hierarchy <= mp.ranks_dict['species']:
 
-                    # Define your SQL query
-                    count_sql_query = f"""
-                        SELECT count(family_ncbi.rfam_acc) 
-                        FROM family_ncbi, taxonomy 
-                        WHERE family_ncbi.ncbi_id = taxonomy.ncbi_id 
-                        AND species LIKE '%{genome_name}%'
-                    """
-                    
-                    # Execute the query
-                    cursor.execute(count_sql_query)
+                        # Define your SQL query
+                        count_sql_query = f"""
+                            SELECT count(family_ncbi.rfam_acc) 
+                            FROM family_ncbi, taxonomy 
+                            WHERE family_ncbi.ncbi_id = taxonomy.ncbi_id 
+                            AND species LIKE '%{genome_name}%'
+                        """
+                        # Execute the query
+                        cursor.execute(count_sql_query)
+                        # Fetch all the rows returned by the query
+                        rows = cursor.fetchall()
+                        families_count = rows[0][0]
+                        print( " genome_name: " + genome_name + " rank_hierarchy: " + str(rank_hierarchy) + " " + rank )
+                        print("  families_count: " + str(families_count) )
+                        if families_count > max_nb_families:
+                            max_nb_families = families_count
+                            families_sql_query = f"""
+                            SELECT family_ncbi.rfam_acc 
+                            FROM family_ncbi, taxonomy 
+                            WHERE family_ncbi.ncbi_id = taxonomy.ncbi_id 
+                            AND species LIKE '%{genome_name}%'
+                            """
+                            cursor.execute(families_sql_query)
+                            rfam_results = set(row[0] for row in cursor)
+            except Exception as e:
+                print("Error connecting to Rfam : " + str(e))
 
-                    # Fetch all the rows returned by the query
-                    rows = cursor.fetchall()
-                    families_count = rows[0][0]
-                    if families_count > max_nb_families:
-                        max_nb_families = families_count
-                        families_sql_query = f"""
-                        SELECT family_ncbi.rfam_acc 
-                        FROM family_ncbi, taxonomy 
-                        WHERE family_ncbi.ncbi_id = taxonomy.ncbi_id 
-                        AND species LIKE '%{genome_name}%'
-                    """
-                        cursor.execute(families_sql_query)
-                        rfam_results = set(row[0] for row in cursor)
-                        print(" loop: " + str(l) + " - has " + str(families_count) + " counts for " + genome_name + " hierarchy: " + str(rank_hierarchy) + " rank: " + rank)
-        
         # Write the results to a file
         with open(rfam_ids_path, "w") as output_file:
             for rf in rfam_results:
                 output_file.write(rf + "\n")
-
         # Close the cursor and connection
         cursor.close()
         connection.close()
-
+        print("rfam ids path: " + rfam_ids_path)
     except mysql.connector.Error as err:
         print("Error connecting to MySQL RFam server: ", err)
 
@@ -86,6 +88,5 @@ if __name__ == "__main__":
         print("** begin RFAM accessions " + str(now) + " genome_name: " + genome_name + " rfam config: " + rfam_config)
         tr = mp.read_tax_rank(genome_name)
         rfam_ids_path = query_Rfam(tr, rfam_config, genome_name)
-        print("rfam path: " + rfam_ids_path)
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print("** end RFAM accessions " + str(now) )
