3b999c2384db3bf82ba68e7c841b065704a1a56c
Manuel Carbajo <mcarbajo@hl-codon-35-04.ebi.ac.uk>
2023-12-12 16:10:37 +0000

    beautifying

--- bin/download_data_from_orthoDB_Swati.py
+++ bin/download_data_from_orthoDB_Swati.py
@@ -19,7 +19,6 @@ def parallize_jobs(jobs,orig_clusters_comb):
         # Use partial to fix the orig_clusters_comb argument
         func = partial(get_sequences, orig_clusters_comb)
         procs.map(func, jobs)
-        #procs.map(get_sequences, jobs)
         procs.close()
         procs.join()
 
@@ -40,7 +39,6 @@ def get_sequences(orig_clusters_comb, data):
 
 def map_retrieve(ncbi_id=None, w_dir=None, orig_clusters_comb=None):
     print("WORKING ON %s" % ncbi_id)
-    # command1 = ["curl", "https://data.orthodb.org/current/search?level=%s&species=%s&limit=10000000"%(ncbi_id, ncbi_id)] # by default only 1000 ids can be downloaded, limit=10000000 is set to overcome it.
     command1 = ["curl", "https://data.orthodb.org/current/search?universal=0.9&singlecopy=0.9&level=%s&species=%s&take=5000"%(ncbi_id, ncbi_id)]
     response = subprocess.Popen(command1, stdout=subprocess.PIPE) # pipe the terminal output to response
     out, err = response.communicate(timeout=5) # Wait for process to terminate
@@ -102,41 +100,20 @@ if __name__ == "__main__":
 
         # Remove duplicate sequences from the original cluster file
         print(f"Removing duplicate sequences from {orig_clusters_comb}")
-        #dedup_command = f"python {baseDir}/bin/remove_dup_seqs.py {orig_clusters_comb}"
-        #dedup_command = f"python {baseDir}/bin/remove_dup_seqs.py {orig_clusters_comb} > {dedup_out_temp}"
         try:
             mp.remove_dup_seqs(orig_clusters_comb, dedup_out_temp)
             print(f"Remove duplicates successful. Output saved to {dedup_out_temp}")
         except subprocess.CalledProcessError as e:
             print(f"Error: Failed to remove duplicates.")
         
-        """
-        with open(dedup_out_temp, 'w') as outfile:
-            try:
-                subprocess.run(dedup_command, shell=True, check=True, stdout=outfile)
-                print(f"Remove duplicates successful. Output saved to {dedup_out_temp}")
-            except subprocess.CalledProcessError as e:
-                print(f"Error: Failed to remove duplicates.")
-                #print(f"Command: {e.cmd}")
-                #print(f"Exit code: {e.returncode}")
-                #print(f"Output: {e.output.decode() if e.output else 'No output'}")
-        """
-
         # Process deduplicated sequences to create a reheadered OrthoDB fasta file
         print("Processing deduplicated seq headers, isolating to unique OrthoDB gene ID...")
-        
-        #reheader_command = f"python {baseDir}/bin/reheader_orthodb.py {dedup_out_temp} {final_ortho_fasta}"
-        #print(f"Command: {reheader_command}\n")
 
         try:
-            #subprocess.run(reheader_command, shell=True)
             mp.rehead_fasta(dedup_out_temp,final_ortho_fasta)
             print(f"Reheading successful. Output saved to {final_ortho_fasta}")
         except subprocess.CalledProcessError as e:
             print(f"Error: Failed to rehead.")
-            #print(f"Command: {e.cmd}")
-            #print(f"Exit code: {e.returncode}")
-            #print(f"Output: {e.output.decode() if e.output else 'No output'}")
         
         # Create a samtools index file
         print(f"Creating samtools index of {final_ortho_fasta}")
@@ -146,9 +123,6 @@ if __name__ == "__main__":
             print(f"Samtools index creation successful for {final_ortho_fasta}")
         except subprocess.CalledProcessError as e:
             print(f"Error: Failed to create Samtools index for {final_ortho_fasta}")
-            #print(f"Command: {e.cmd}")
-            #print(f"Exit code: {e.returncode}")
-            #print(f"Output: {e.output.decode() if e.output else 'No output'}")
 
         # Clean up temporary files
         for filename in os.listdir(d_dir):
